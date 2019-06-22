c58f542c1ec18e0ac7d8dccaba746c71ca9a4160
Denis Defreyne <denis.defreyne@stoneship.org>
2019-06-22 13:35:11 +0200

    Use docs/ for compiled project for now

---
+++ docs/precache-manifest.2cb684fe56bb237702ec171f178692a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056cd16c086988f9c6f26fe06901ee8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "904b0bc5bb35bbc87132",
+    "url": "/static/css/main.410ca877.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "904b0bc5bb35bbc87132",
+    "url": "/static/js/main.8fdbaf29.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
