556612bf194095b7fcfe5be7afdd571e52308707
Denis Defreyne <denis.defreyne@stoneship.org>
2019-06-22 13:39:47 +0200

    Update homepage

---
+++ docs/precache-manifest.6605f2f7f80dd880fe1fba31f65ffc53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be99c2a666449da55fd9a9b2d8dcf7d",
+    "url": "/portfolio1/index.html"
+  },
+  {
+    "revision": "bba4f3eb5936f7563bd2",
+    "url": "/portfolio1/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portfolio1/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "bba4f3eb5936f7563bd2",
+    "url": "/portfolio1/static/js/main.6e414840.chunk.js"
+  },
+  {
+    "revision": "4b1a731c406f33d2afd7",
+    "url": "/portfolio1/static/js/runtime~main.5da598bc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio1/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2cb684fe56bb237702ec171f178692a5.js"
+  "/portfolio1/precache-manifest.6605f2f7f80dd880fe1fba31f65ffc53.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
